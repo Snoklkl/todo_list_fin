8f1806f73f5dfba07e4c88448bf9ef8b8d4e923f
Noah <Snoklkl@users.noreply.github.com>
2021-05-16 18:29:00 -0500

    input components js written

---
+++ src/components/input.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+import './input.css'
+
+import  {useDispatch}  from 'react-redux'
+import saveToDo from '../features/todos/todosSlice';
+
+const Input = () => {
+    const [input , setInput ] = useState('')
+    
+    const dispatch = useDispatch()  
+
+    const addToDo = () => {
+        console.log("Add" + input)
+
+        dispatch(saveToDo({
+            
+
+            item: input,
+            done: false,
+            id: Date.now()
+
+
+         } ) )
+    }
+
+    return (
+        <div className='input'>
+
+            <input type="text" value={input} onChange={e => setInput(e.target.value)} /> 
+            <button onClick={addToDo}> Add Task </button>
+       
+
+        </div>
+    )
+}
+ 
+
+export default Input
