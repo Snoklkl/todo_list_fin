983dae72a8c0d3ae1bf911df7fe1d53274dc11ef
Noah <Snoklkl@users.noreply.github.com>
2021-05-16 18:31:09 -0500

    added input component to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Input from './components/input.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <Input />
     </div>
   );
 }
