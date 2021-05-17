6514c81094509eaecccb99aa68560989bf139ddc
Noah <Snoklkl@users.noreply.github.com>
2021-05-17 17:06:39 -0500

    added working todoitems and check boxes though not where i want them

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
                />)}
               )
               }
-              <ToDo />
+            
 
           </div>
           <Input />   
--- src/components/todo.js
+++ src/components/todo.js
@@ -9,8 +9,16 @@ const ToDo = ( {name, done, id}) => {
     return (   
         <div className='todoList'>
             
+            <div className={`list_item ${done ? "completed_item" : ""} 
+              `} >
+  
+                  {name}
 
-            <p> {name} </p>
+
+                
+            </div>
+            <input class="form-check-input" type="checkbox" id="checkboxNoLabel" value="" aria-label="..."></input>
+
         </div>
     )
 
