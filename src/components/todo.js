c3652b0a8e123f785ccb300b992ec749a4dc8b73
Noah <Snoklkl@users.noreply.github.com>
2021-05-17 23:54:33 -0500

    turned todos into buttons

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,23 +1,34 @@
 import React from 'react'
 import './todo.css'
+import  {useDispatch}  from 'react-redux';
 
 
 const ToDo = ( {name, done, id}) => {
 
+    const itemDone = () => {
+        if ({done} === true){
+            document.getElementById('toggle').style.textDecoration = "strike-through";
+            
+        }
+        else {
+            document.getElementById('toggle').style.textDecoration = "none";
+        }
+    }
 
 
     return (   
         <div className='todoList'>
             
-            <div className={`list_item ${done ? "completed_item" : ""} 
-              `} >
+            <button id='toggle' onClick={itemDone} className={'list_item'} >
   
                   {name}
+                  
 
+               </button>
+ 
+                                        
+                            
 
-                
-            </div>
-            <input class="form-check-input" type="checkbox" id="checkboxNoLabel" value="" aria-label="..."></input>
 
         </div>
     )
