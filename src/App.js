e2d5ecfc5889ac02e6788e5ca500cf12ea196885
Noah <Snoklkl@users.noreply.github.com>
2021-05-19 11:13:17 -0500

    edited some form values to be make more sense

--- src/App.js
+++ src/App.js
@@ -131,16 +131,17 @@ function Contact() {
 
       <form>
           <div class="form-group">
-          <label for="exampleInputEmail1"> Name </label>
-          <input type="email" class="form-control" id="Name" aria-describedby="emailHelp" placeholder="Enter your name" />
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          <label for="NameInput"> Name </label>
+          <input type="text" class="form-control" id="Name" aria-describedby="emailHelp" placeholder="Enter your name" />
+          
           </div>
         <div class="form-group">
-          <label for="exampleInputPassword1">Email</label>
-          <input type="password" class="form-control" id="Email" placeholder="Enter your email" />
+          <label for="EmailInput">Email</label>
+          <input type="email" class="form-control" id="Email" placeholder="Enter your email" />
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
-          <label  for="exampleCheck1">Say hi!</label>
+          <label  for="Textarea">Say hi!</label>
           <textarea type="text" rows="3" class="form-control" id="Message" > </textarea>
         
         </div>
