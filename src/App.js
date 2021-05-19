47d141bdc0d1015fd8dfbfeedad489897daf8dc4
Noah <Snoklkl@users.noreply.github.com>
2021-05-19 01:20:24 -0500

    added various different side pages

--- src/App.js
+++ src/App.js
@@ -128,8 +128,24 @@ function Contact() {
     <div className='container'>
       
       <div className='todo_container'>
+
+      <form>
+          <div class="form-group">
+          <label for="exampleInputEmail1"> Name </label>
+          <input type="email" class="form-control" id="Name" aria-describedby="emailHelp" placeholder="Enter your name" />
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Email</label>
+          <input type="password" class="form-control" id="Email" placeholder="Enter your email" />
+        </div>
+        <div class="form-group">
+          <label  for="exampleCheck1">Say hi!</label>
+          <textarea type="text" rows="3" class="form-control" id="Message" > </textarea>
         
-     
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
         
 
       </div>
